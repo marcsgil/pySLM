6a9542cd1ebb00b2d78b1f0e07a97f82ae08bb64
Marcos Gil <marcosgildeoliveira@gmail.com>
2023-06-05 19:21:03 -0300

    added slm configs

--- src/pySLM/test.py
+++ src/pySLM/test.py
@@ -2,12 +2,17 @@ import numpy as np
 import slmpy
 import initial_profiles
 
-slm = slmpy.SLMdisplay(monitor=0)
+#All the distances are in mm
+#The SLM parameters are adjusted for a Hamamatsu X10468
+width = 15.8
+height = 12
+
+slm = slmpy.SLMdisplay(monitor = 1)
 resX, resY = slm.getSize()
-delta = 3 / resY
-X,Y = np.meshgrid(np.linspace(-delta * resX,delta * resX,resX),np.linspace(-3,3,resY))
-input = initial_profiles.hg(X,Y,0,0,5)
-desired = initial_profiles.lg(X,Y,1,2,.1)
+X,Y = np.meshgrid(np.linspace(-width/2 , width/2 ,resX),np.linspace(-height/2 ,height/2 ,resY))
+offset = 0
+input = initial_profiles.hg(X-offset,Y,0,0,10)
+desired = initial_profiles.diagonal_hg(X-offset,Y,3,3,.3)
 
 holo = initial_profiles.generate_hologram(desired,input,X,Y,.1)
 
